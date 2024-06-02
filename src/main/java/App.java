6607540876f32d5f802c1ccfbbf01bf76b8ab75c
God-I-Tried <zz022@yandex.ru>
2024-06-03 01:11:37 +0300

    create

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+        System.out.println(bean.equals(beanTwo));
+        Cat catBeanOne =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBeanOne.getMessage());
+        Cat catBeanTwo =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBeanTwo.getMessage());
+        System.out.println(catBeanOne.equals(catBeanTwo));
     }
 }
